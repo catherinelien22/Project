747f792f0254f8910490678a5c2c4228138de871
jeremyt19115678 <jeremyt19115678@tas.tw>
2017-12-02 16:51:47 +0800

    Added User Class
    Class subject to change

--- User.java
+++ User.java
@@ -1,8 +1,22 @@
 
 public class User
 {
-    int points, life, r, c, orientation;
-    User() {
-        
+    public int points, life, r, c, orientation;
+    //subject to change
+    public User(int a, int b, int o) {
+        r= a;
+        c = b;
+        orientation = o;
+        life = 2;
+    }
+    
+    public void die(){
+        life--;
+        if ( life >=0)
+            resurrect();
+    }
+    
+    public void resurrect(){
+        //not implemented yet
     }
 }
